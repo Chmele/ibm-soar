4d93a68f8bf99d6c59522d0d01267caddee3ab6d
Chmele <53478956+Chmele@users.noreply.github.com>
2025-08-12 02:07:48 +0300

    doc: stomp misc

--- soar/stomp_functions.go
+++ soar/stomp_functions.go
@@ -5,11 +5,13 @@ import (
 	"log/slog"
 )
 
+// A FunctionCallHandler that is logging the fact of a message recieving
 func LoggingResponse(c *FunctionCall) (*FuncResponse, error) {
 	slog.Info("Recieved function call", slog.String("function_name", c.Function.Name))
 	return nil, nil
 }
 
+// A FunctionCallHandler updating the status of a playbook instance currently running a function that a function run is started
 func StartedResponse(*FunctionCall) (*FuncResponse, error) {
 	return &FuncResponse{
 		MessageType: 0,
@@ -18,6 +20,7 @@ func StartedResponse(*FunctionCall) (*FuncResponse, error) {
 	}, nil
 }
 
+// A FunctionCallHandler updating the status of a playbook instance currently running a function with result response
 func CompletedResponse(c *FunctionCall) (*FuncResponse, error) {
 	return &FuncResponse{
 		MessageType: 2,
@@ -31,6 +34,7 @@ func CompletedResponse(c *FunctionCall) (*FuncResponse, error) {
 	}, nil
 }
 
+// Not implemented
 type FunctionLookup struct {
 	mapping map[string]FunctionCallHandler
 }
--- soar/stomp_options.go
+++ soar/stomp_options.go
@@ -6,34 +6,44 @@ import (
 	"log/slog"
 )
 
-
 type StompOption func(*StompListener) error
-type StompOpts struct {}
+type StompOpts struct{}
+
 var Stomp = StompOpts{}
+
+// Message destination api name
 func (StompOpts) MessageDestination(md string) func(*StompListener) error {
 	return func(l *StompListener) error {
 		l.MessageDestination = md
 		return nil
 	}
 }
+
+// Port to be used in a STOMP connection, 65001 by default
 func (StompOpts) Port(port int) func(*StompListener) error {
 	return func(l *StompListener) error {
 		l.StompPort = fmt.Sprint(port)
 		return nil
 	}
 }
+
+// Context to be used in a listening
 func (StompOpts) Context(ctx context.Context) func(*StompListener) error {
 	return func(l *StompListener) error {
 		l.Ctx = ctx
 		return nil
 	}
 }
+
+// Whether to trust the self-signed certificate or not
 func (StompOpts) Insecure(insecure bool) func(*StompListener) error {
 	return func(l *StompListener) error {
 		l.Insecure = insecure
 		return nil
 	}
 }
+
+// Structured logger to be used for runtime logs
 func (StompOpts) Logger(logger *slog.Logger) func(*StompListener) error {
 	return func(l *StompListener) error {
 		l.Logger = logger
