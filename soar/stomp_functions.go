47edc8133b21833a63103a8ee182e684965133f3
Chmele <53478956+Chmele@users.noreply.github.com>
2025-08-13 00:26:43 +0300

    feat: more response variations

--- soar/stomp_functions.go
+++ soar/stomp_functions.go
@@ -1,8 +1,11 @@
 package soar
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log/slog"
+
 	"github.com/chmele/ibm-soar/soar/structures"
 )
 
@@ -35,6 +38,50 @@ func CompletedResponse(c *structures.FunctionCall) (*structures.FuncResponse, er
 	}, nil
 }
 
+func ErrorResponse(c *structures.FunctionCall, err error) *structures.FuncResponse {
+	return &structures.FuncResponse{
+		MessageType: 3,
+		Message:     fmt.Sprintf("Error occured: %v", err),
+		Complete:    true,
+		Results: &structures.Results{
+			Version: 2.0,
+			Success: false,
+			Content: nil,
+		},
+	}
+}
+
+func SuccessResponse(text string) *structures.FuncResponse {
+	return &structures.FuncResponse{
+		MessageType: 0,
+		Message:     "App function completed",
+		Complete:    true,
+		Results: &structures.Results{
+			Version: 2.0,
+			Success: true,
+			Content: text,
+		},
+	}
+}
+
+func LoadInputs[Input any](fc *structures.FunctionCall) (*Input, error) {
+	var inputMap map[string]any
+	if m, ok := fc.Inputs.(map[string]any); ok {
+		inputMap = m
+	} else {
+		return nil, fmt.Errorf("Function input is not a map of strings")
+	}
+	data, err := json.Marshal(inputMap)
+	if err != nil {
+		return nil, err
+	}
+	var ret Input
+	if err := json.NewDecoder(bytes.NewReader(data)).Decode(&ret); err != nil {
+		return nil, err
+	}
+	return &ret, nil
+}
+
 // Not implemented
 type FunctionLookup struct {
 	mapping map[string]FunctionCallHandler
@@ -44,7 +91,7 @@ func (l *FunctionLookup) Register(name string, handler FunctionCallHandler) {
 	l.mapping[name] = handler
 }
 
-func(l *FunctionLookup) Handler(c *structures.FunctionCall) (*structures.FuncResponse, error) {
+func (l *FunctionLookup) Handler(c *structures.FunctionCall) (*structures.FuncResponse, error) {
 	f, ok := l.mapping[c.Function.Name]
 	if !ok {
 		return nil, fmt.Errorf("Got a call with unregistered function name: %s", c.Function.Name)
